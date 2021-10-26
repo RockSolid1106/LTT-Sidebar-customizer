ffc47b7fac8b614e0ec4e39b6afebd66595dc3ae
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-10-26 13:05:51 +0530

    Update content_script.js

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -25,16 +25,7 @@ chrome.storage.sync.get({
                     element3.remove()
                 }
                 
-            
 
-        //     try{
-        //         //*[@id="ipsLayout_sidebar"]/div[2]/ul/li[2]
-        // let element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        // element3.singleNodeValue.remove()
-        //     } catch(ex){
-        // element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        // element3.singleNodeValue.remove()
-        //     }
         }
 
         
@@ -53,37 +44,25 @@ chrome.storage.sync.get({
                 element2.remove()
             }
             
-        // try {    
-        //     //*[@id="ipsLayout_sidebar"]/div[2]/ul/li[3]
-        //     //*[@id="ipsLayout_sidebar"]/div[3]/ul/li[3]
 
-            
-
-        // // let element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        // // element2.singleNodeValue.remove()
-        // // } catch(ex){
-        
-        // // element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        // // element2.singleNodeValue.remove()
-        // }
         }
         
         if (items.vid == true){
             
             try {
-            var element = document.querySelectorAll('div.lmg-card');
-            for (let elem of element){
-            elem.parentElement.removeChild(elem);
-            } 
-        }catch(ex){}
+                var element = document.querySelectorAll('div.lmg-card');
+                for (let elem of element){
+                    elem.parentElement.removeChild(elem);
+                } 
+            }catch(ex) {}
         }
         
         if (items.ad == true){
 
             try{
-            var element5 = document.querySelector("div[data-role='sidebarAd']")
-            element5.remove()
-            } catch(ex){}
+                var element5 = document.querySelector("div[data-role='sidebarAd']")
+                element5.remove()
+            } catch(ex) {}
         }
         
   })
