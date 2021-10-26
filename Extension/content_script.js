58ee30f81dd13a18b0d678c8c297b21bf88ce842
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-10-26 10:32:14 +0530

    Fixed some problems
    Fixed an error where the extension won't work on topics with the top poster analytics.

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -11,31 +11,46 @@ chrome.storage.sync.get({
 
 
         if (items.feat==true){
-
+            try{
+                
         let element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
         element3.singleNodeValue.remove()
+            } catch(ex){}
+            try{
+        element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        element3.singleNodeValue.remove()
+            } catch(ex){}
         }
 
         
         if (items.social==true){
 
-            
+        try {    
         let element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
         element2.singleNodeValue.remove()
+        } catch(ex){}
+        try{
+        element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        element2.singleNodeValue.remove()
+        } catch(ex){}
         }
         
         if (items.vid == true){
             
-
+            try {
             var element = document.querySelectorAll('div.lmg-card');
             for (let elem of element){
             elem.parentElement.removeChild(elem);
+            } 
+        }catch(ex){}
         }
-        }
+        
         if (items.ad == true){
-            console.log("running removead")
+
+            try{
             var element5 = document.querySelector("div[data-role='sidebarAd']")
             element5.remove()
+            } catch(ex){}
         }
         
   })
