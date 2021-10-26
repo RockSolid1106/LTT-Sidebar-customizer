7cb7eef3efef3308ed52e0ba1a91b7f832cf12e8
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-10-26 12:40:31 +0530

    Fixed an essentially broken extension

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -11,28 +11,61 @@ chrome.storage.sync.get({
 
 
         if (items.feat==true){
-            try{
+            
+                let element3 = document.querySelector('li[data-blockid="app_forums_topicFeed_yxyq4buu6"]')
+                if (element3 != null){
+                element3.remove()
+                }
+                else if (document.querySelector('li[data-blockid="app_forums_topicFeed_dpltxl1vm"]') != null){
+                    element3 = document.querySelector('li[data-blockid="app_forums_topicFeed_dpltxl1vm"]')
+                    element3.remove()
+                }
+                else {
+                    element3 = document.querySelector('li[data-blockid="app_forums_topicFeed_6pz746tdr"]')
+                    element3.remove()
+                }
                 
-        let element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        element3.singleNodeValue.remove()
-            } catch(ex){}
-            try{
-        element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        element3.singleNodeValue.remove()
-            } catch(ex){}
+            
+
+        //     try{
+        //         //*[@id="ipsLayout_sidebar"]/div[2]/ul/li[2]
+        // let element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        // element3.singleNodeValue.remove()
+        //     } catch(ex){
+        // element3 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        // element3.singleNodeValue.remove()
+        //     }
         }
 
         
         if (items.social==true){
+            
+            let element2 = document.querySelector('li[data-blockid="app_cms_Blocks_lwnptbsqn"]')
+            if (element2 != null){
+            element2.remove()
+            }
+            else if(document.querySelector('li[data-blockid="app_cms_Blocks_vygscspy7"]') != null){
+                element2 = document.querySelector('li[data-blockid="app_cms_Blocks_vygscspy7"]')
+                element2.remove()
+            }
+            else {
+                element2 = document.querySelector('li[data-blockid="app_cms_Blocks_6rae4q53s"]')
+                element2.remove()
+            }
+            
+        // try {    
+        //     //*[@id="ipsLayout_sidebar"]/div[2]/ul/li[3]
+        //     //*[@id="ipsLayout_sidebar"]/div[3]/ul/li[3]
 
-        try {    
-        let element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        element2.singleNodeValue.remove()
-        } catch(ex){}
-        try{
-        element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-        element2.singleNodeValue.remove()
-        } catch(ex){}
+            
+
+        // // let element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[2]/ul/li[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        // // element2.singleNodeValue.remove()
+        // // } catch(ex){
+        
+        // // element2 = document.evaluate("//*[@id='ipsLayout_sidebar']/div[3]/ul/li[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+        // // element2.singleNodeValue.remove()
+        // }
         }
         
         if (items.vid == true){
