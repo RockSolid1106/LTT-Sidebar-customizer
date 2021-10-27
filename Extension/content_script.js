d0df150ffb12b21c2352fdf2b2aa0901c4bca841
Rock <84492239+RockSolid1106@users.noreply.github.com>
2021-10-27 20:12:06 +0530

    Added GNU license and Copyright info
    Added GNUPL and Copyright information to all the files.

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -1,3 +1,17 @@
+/* This Chrome extension easily allows you to customize what is shown on the sidebar on the LTT Forum.
+Copyright © 2021  RockSolid106
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. */
+
+
 var vid=false;
 var soc=false;
 var feat=false;
--- Extension/popup.js
+++ Extension/popup.js
@@ -1,3 +1,14 @@
+// Copyright © 2021  RockSolid106
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details. 
 
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
