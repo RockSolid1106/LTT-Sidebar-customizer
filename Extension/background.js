7177c0b5170d3ffd4d84caa7dab8606dc601a1d5
Rock <84492239+RockSolid1106@users.noreply.github.com>
2022-04-02 11:48:56 +0530

    Improvements to webRequest event listener

--- Extension/background.js
+++ Extension/background.js
@@ -1,10 +1,20 @@
-var target = "https://linustechtips.com/topic/*";
+const target = "https://linustechtips.com/topic/*";
 
-function logResponse(responseDetails) { 
-      chrome.tabs.executeScript({file: "topics.js"})
-        }
+
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+async function logResponse(responseDetails) { 
+  let tab = await getCurrentTab();
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ["topics.js"]})
+}
 
 chrome.webRequest.onCompleted.addListener(
   logResponse,
-  {urls: ["https://linustechtips.com/topic/*"]}
+  {urls: [target]}
 );
